a10d55d8c7ebcddb0cf7303596aaa2ccde9380af
Benji Max Levine <benji@benjilevine.com>
2020-06-01 13:26:52 +0100

    Rewrite setup

--- setup.py
+++ setup.py
@@ -4,31 +4,25 @@ from setuptools import setup, find_packages
 from boggle_web.exceptions import EnvironmentUnsetError
 
 required_env_vars = [
-    'WFB_PROJECT_NAME',
-    'WFB_FLASK_SECRET_KEY'
+    'FLASK_SECRET_KEY'
 ]
 for var in required_env_vars:
     if var not in environ:
         raise EnvironmentUnsetError(var)
 
-optional_setup = {}
-if 'WFB_AUTHOR_NAME' in environ:
-    optional_setup['author'] = environ['WFB_AUTHOR_NAME']
-if 'WFB_AUTHOR_EMAIL' in environ:
-    optional_setup['author_email'] = environ['WFB_AUTHOR_EMAIL']
-if 'WFB_AUTHOR_NAME' in environ:
-    optional_setup['url'] = environ['WFB_PROJECT_URL']
-
 setup(
-    name=environ['WFB_PROJECT_NAME'],
+    name='Letter Shuffle Game',
     version='0.1',
     long_description=__doc__,
     packages=find_packages(),
     include_package_data=True,
     zip_safe=True,
+    author='Benji Max Levine',
+    author_email='benji@benjilevine.com',
+    url='https://github.com/benjilev08/boggle-web',
     install_requires=[
         'datetime',
-        'Flask'
-    ],
-    **optional_setup
+        'Flask',
+        'tabulate'
+    ]
 )
